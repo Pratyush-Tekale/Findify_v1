e6b90f6949b0f605ed700410fc372ad8aa35f89d
Pratyush-Tekale <tekalepratyush@gmail.com>
2026-07-25 23:19:40 +0530

    Added DatabaseConnection

---
+++ src/main/java/com/findify/util/DBConnection.java
@@ -0,0 +1,35 @@
+package com.findify.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/findify_db";
+
+    private static final String USER =
+            "root";
+
+    private static final String PASSWORD =
+            "0000";
+
+    public static Connection getConnection() {
+
+        Connection con = null;
+
+        try {
+
+            con = DriverManager.getConnection(URL, USER, PASSWORD);
+
+            System.out.println("Database Connected Successfully!");
+
+        } catch (SQLException e) {
+
+            e.printStackTrace();
+        }
+
+        return con;
+    }
+}
